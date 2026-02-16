6029ff5a6174952fbb74c7ae7021b20a3f0afab2
Sota Hatakeyama <choo.bla.rin@gmail.com>
2026-02-16 10:04:17 +0900

    Initial Commit

---
+++ OverHyper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OverHyper
+//
+//  Created by Sota Hatakeyama on 2026/02/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OverHyper/OverHyperApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverHyperApp.swift
+//  OverHyper
+//
+//  Created by Sota Hatakeyama on 2026/02/16.
+//
+
+import SwiftUI
+
+@main
+struct OverHyperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
